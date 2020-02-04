cb0894b4feab66cefd1b8cb933cda8263797c508
Kaderhome2 <mmkader@dmacc.edu>
2020-02-03 23:57:34 -0600

    module3

--- format_output/average_score.py
+++ format_output/average_score.py
@@ -1,9 +1,15 @@
 def average(avgNum):
     return (avgNum)
 
+
 if __name__ == '__main__':
-    score1 = float(input("Enter score1:"))
-    score2 = float(input("Enter score2:"))
-    score3 = float(input("Enter score3:"))
-    avgNum = (score1 + score2+ score3)/3
-    print(avgNum)
+    last_name = str(input("Enter last name:"))
+    first_name = str(input("Enter first name:"))
+    score1 = float(int(input("Enter score1:")))
+    score2 = float(int(input("Enter score2:")))
+    score3 = float(int(input("Enter score3:")))
+    avgNum = (score1 + score2 + score3)/3
+    average_scores = avgNum
+    print(("Hello, {first_name} {last_name} this is {1:3f}.") \
+            .format(first_name=first_name, last_name=last_name,  \
+                   average_scores=average_scores))
