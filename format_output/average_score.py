940cc783eff8cc165e1d1c3cf08954ff70eb7cfd
Kaderhome2 <mmkader@dmacc.edu>
2020-02-04 23:25:16 -0600

    module3

--- format_output/average_score.py
+++ format_output/average_score.py
@@ -1,15 +1,30 @@
-def average(avgNum):
-    return (avgNum)
+from statistics import mean
+
+"""Return average of three numbers program """
+def average():
+    score1 = int(input("Input score1:"))
+    score2 = int(input("Input score2:"))
+    score3 = int(input("Input score3:"))
+    num_avg = mean([score2, score2, score3])
+    average_score = num_avg
+    return average_score
+
+def get_fname():
+    return (input("Please enter your first name:")).capitalize()
+
+def get_lname():
+    return (input("Please enter your last name:")).capitalize()
 
 
 if __name__ == '__main__':
-    last_name = str(input("Enter last name:"))
-    first_name = str(input("Enter first name:"))
-    score1 = float(int(input("Enter score1:")))
-    score2 = float(int(input("Enter score2:")))
-    score3 = float(int(input("Enter score3:")))
-    avgNum = (score1 + score2 + score3)/3
-    average_scores = avgNum
-    print(("Hello, {first_name} {last_name} this is {1:3f}.") \
-            .format(first_name=first_name, last_name=last_name,  \
-                   average_scores=average_scores))
+   #
+   first_name = get_fname()
+       #str.capitalize(input("Please enter your first name:"))
+   last_name = get_lname()
+   age = int(input("Please enter your age:"))
+average_score = str(round(average(),2))
+print(f'{last_name}, {first_name}, age: {age}, average grade: {average_score}')
+
+#can also add a negative number
+#average of negative numbers is calculated accurately
+#when adding string for age python gives an error
