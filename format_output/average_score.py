22ad934e5f98a2689dc67e484b08ae82c0bb0de4
Kaderhome2 <mmkader@dmacc.edu>
2020-02-05 20:50:27 -0600

    module3

--- format_output/average_score.py
+++ format_output/average_score.py
@@ -1,30 +1,32 @@
 from statistics import mean
 
 """Return average of three numbers program """
+
+
 def average():
     score1 = int(input("Input score1:"))
     score2 = int(input("Input score2:"))
     score3 = int(input("Input score3:"))
-    num_avg = mean([score2, score2, score3])
-    average_score = num_avg
-    return average_score
+    numavg = mean([score1, score2, score3])
+    return numavg
+
 
 def get_fname():
     return (input("Please enter your first name:")).capitalize()
+
 
 def get_lname():
     return (input("Please enter your last name:")).capitalize()
 
 
 if __name__ == '__main__':
-   #
-   first_name = get_fname()
-       #str.capitalize(input("Please enter your first name:"))
-   last_name = get_lname()
-   age = int(input("Please enter your age:"))
-average_score = str(round(average(),2))
-print(f'{last_name}, {first_name}, age: {age}, average grade: {average_score}')
+    first_name = get_fname()
+    # str.capitalize(input("Please enter your first name:"))
+    last_name = get_lname()
+    age = int(input("Please enter your age:"))
+    numavg = str(round(average(), 2))
+print(f'{last_name}, {first_name}, age: {age}, average grade: {numavg}')
 
-#can also add a negative number
-#average of negative numbers is calculated accurately
-#when adding string for age python gives an error
+# can also add a negative number
+# average of negative numbers is calculated accurately
+# when adding string for age python gives an error
--- test/test_average_scores.py
+++ test/test_average_scores.py
@@ -3,9 +3,10 @@ import unittest.mock as mock
 from format_output import average_score as topic2
 
 
+
 def test_average(self):
-        with mock.patch('builtins.input', side_effect=[85, 90, 95]):
-            assert topic2.average() == 90
+    with mock.patch('builtins.input', side_effect=[85, 90, 95]):
+        assert topic2.average() == 90
 
 if __name__ == '__main__':
     unittest.main()
